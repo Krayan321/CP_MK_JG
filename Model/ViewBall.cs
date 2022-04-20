75b982b96e1be977989bc033266372ff9b2a5a9b
Mateusz Kowalski <236567@edu.p.lodz.pl>
2022-04-20 23:36:06 +0200

    ViewBall getters

---
+++ Model/ViewBall.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Logic;
+
+namespace ViewModel
+{
+    public class ViewBall
+    {
+        private readonly Ball logicBall;
+
+        public float Position_X
+        {
+            get { return logicBall.Position_X; }
+        }
+        public float Position_Y
+        {
+            get { return logicBall.Position_Y; }
+        }
+        public float[] Direction
+        {
+            get { return logicBall.Direction; }
+        }
+        public float Speed
+        {
+            get { return logicBall.Speed; }
+        }
+        public int Radius
+        {
+            get { return logicBall.Radius; }
+        }
+
+        public ViewBall(Ball logicBall)
+        {
+            this.logicBall = logicBall;
+        }
+
+
+
+
+    }
+}

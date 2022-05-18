07d8f910d02aba980768179fdec0a6b2ac9c0529
Krayan321 <krayan321@gmail.com>
2022-05-19 00:01:08 +0200

    Vector start

---
+++ Logic/Vectors.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Logic
+{
+    public struct Vectors
+    {
+        public static readonly Vectors Zero = new Vectors(0, 0);
+        public static readonly Vectors One = new Vectors(1, 1);
+        public static readonly Vectors Up = new Vectors(0f, 1f);
+        public static readonly Vectors Down = new Vectors(0f, -1f);
+        public static readonly Vectors Left = new Vectors(-1f, 0f);
+        public static readonly Vectors Right = new Vectors(1f, 0f);
+
+        public float X { get; set; }
+        public float Y { get; set; }
+
+        public Vectors(double x, double y)
+        {
+            X = (float)x;
+            Y = (float)y;
+        }
+
+        public static float Distance(Vectors point, Vectors point1)
+        {
+            return (float)Math.Sqrt(DistanceSquared(point, point1));
+        }
+
+        public static float DistanceSquared(Vectors point, Vectors point1)
+        {
+            float xDifference = point.X - point1.X;
+            float yDifference = point.Y - point1.Y;
+            return xDifference * xDifference + yDifference * yDifference;
+        }
+
+        public bool IsZero()
+        {
+            return Equals(Zero);
+        }
+
+        
+
+       
+    }
+}
